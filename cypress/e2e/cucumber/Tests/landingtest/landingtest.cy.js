16981b9973b7faa6605733940c4a59c3de68cb63
DESTINYAS <destiny.ochonogor@gmail.com>
2024-04-12 17:52:13 +0100

    Add step - switch windows

--- cypress/e2e/cucumber/Tests/landingtest/landingtest.cy.js
+++ cypress/e2e/cucumber/Tests/landingtest/landingtest.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-const {Given, When, Then, And} =require("cypress-cucumber-preprocessor/steps")
+const {Given, When, Then, And} = require("cypress-cucumber-preprocessor/steps")
 const LandingPage = require("../../Pages/Landing/landing.cy");
 
 Given("I navigate to the Amazon landing page", () => {
@@ -16,7 +16,7 @@ LandingPage.clickTabletAccesoriesLinkItem();
 });
 And("Check The JETech checkbox", () => {
     // This step is indeterminate as the JETtech checkbox does not always show when the page loads and this could stop the test
-    
+
 // LandingPage.checkJETechBox();
 })
 And("Click on the Newest Arrivals option", () => {
@@ -26,7 +26,11 @@ And("Click on the Lowest Priced item", () => {
     LandingPage.selectLowestPricedItem();
 });
 And("Switch Window", () => {
-
+    cy.window().then(win => {
+        cy.location().then(loc=> {
+        win.open(loc.href, '_blank'); 
+        })
+      });
 })
 Then("'About this Item' section should be present on the page", () => {
     LandingPage.confirmAboutItem();
